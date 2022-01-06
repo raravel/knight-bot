aa1dc7d115dd6c9f7aec364c49a7d67450716ad5
raravel <youn@tree-some.dev>
2022-01-06 21:24:45 +0900

    소라고동 엔진 변경

--- src/command/magic-conch.ts
+++ src/command/magic-conch.ts
@@ -14,38 +14,77 @@ import { CommandMessage } from '.';
 import { cooker } from '../common';
 import qs from 'querystring';
 
+const USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.272 Whale/2.9.118.16 Safari/537.36';
+
 export default {
 	command: '소라고동',
 	permission: null,
 	async run(cmd: CommandMessage, client: Client) {
-		let res: any = await got.get('https://kr.shindanmaker.com/329887');
-		let $: any = cheerio.load(res.body);
-
+		let res: any = await got.get('http://m.tf.co.kr/sa2da/step1?idx=92', {
+			headers: {
+				'user-agent': USER_AGENT
+			},
+		});
 		const cookie = cooker(res.headers['set-cookie']);
-		const session = cookie._session.value;
-		const token = $('#shindanForm').children('input[type=hidden]').val();
+		const session = cookie['PHPSESSID'].value;
 
-		res = await got.post('https://kr.shindanmaker.com/329887', {
+		res = await got.post('http://m.tf.co.kr/sa2da/ajaxcheck', {
 			body: qs.encode({
-				'_token': token,
-				'shindanName': Date.now()+cmd.content,
-				'hiddenName': `무명 ${String.fromCharCode(65 + Math.floor(Math.random()*10))}`,
+				idx: 92,
+				page: 0,
+				qaid: 'ITEM_1485404592650',
+				cdata: `ITEM_1485404592650_E_0|22|${cmd.content}`,
 			}),
 			headers: {
-				cookie: `_session=${session};`,
-				'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.272 Whale/2.9.118.16 Safari/537.36',
-				'content-type': 'application/x-www-form-urlencoded',
+				cookie: `PHPSESSID=${session};`,
+					'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+				'user-agent': USER_AGENT,
 			},
-			followRedirect: false,
 		});
-		$ = cheerio.load(res.body);
-		const resText = $('#shindanResult').text();
-		if ( resText ) {
-			const m = resText.match(/'(.*?)'.*?'(.*?)'/) as any;
+
+		res = await got.get('http://m.tf.co.kr/sa2da/step2?idx=92&page=0', {
+			headers: {
+				cookie: `PHPSESSID=${session};`,
+					'user-agent': USER_AGENT
+			},
+		});
+
+		res = await got.post('http://m.tf.co.kr/sa2da/ajaxcheck', {
+			body: qs.encode({
+				idx: 92,
+				page: 1,
+				qaid: 'ITEM_1485409103810',
+				cdata: 'ITEM_1485409103810_E_0',
+			}),
+			headers: {
+				cookie: `PHPSESSID=${session};`,
+					'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+				'user-agent': USER_AGENT,
+			},
+		});
+
+		const check = JSON.parse(res.body);
+		if ( check.status === 'sucess' ) {
+			res = await got.get('http://m.tf.co.kr/sa2da/step2?idx=92&page=1', {
+				headers: {
+					cookie: `PHPSESSID=${session};`,
+						'user-agent': USER_AGENT
+				},
+			});
+			let m: any = res.body.match(/.*?'.(.*)?'/);
 			if ( m ) {
-				const reply: string = m[2];
-				await cmd.message.reply(reply);
-				return '';
+				const url = `http://m.tf.co.kr/sa2da${m[1]}`;
+					res = await got.get(url, {
+					headers: {
+						cookie: `PHPSESSID=${session};`,
+					},
+				});
+				let $: any = cheerio.load(res.body);
+				m = $('#cutresult').text().match(/.*?"(.*)?"/);
+				if ( m ) {
+					await cmd.message.reply(m[1]);
+					return '';
+				}
 			}
 		}
 		await cmd.message.reply('몰?루');
